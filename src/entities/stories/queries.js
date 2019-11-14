3a2d713882576764c897b6a545300c7bf54325be
Pedro Lopes <pedroyremolo@gmail.com>
2019-11-14 00:16:17 -0300

    [CHANGE] Correct query single quote issue

--- src/entities/stories/queries.js
+++ src/entities/stories/queries.js
@@ -1,11 +1,11 @@
 export const query = () => {
   return {
     post: `INSERT INTO historia_quadrinho (img_url, data_publicacao, titulo) 
-            VALUES ("?", ?, "?");`,
+            VALUES (?, ?, ?);`,
     get: `SELECT * FROM historia_quadrinho;`,
     getById: `SELECT * FROM historia_quadrinho WHERE id_hq = ?;`,
     putById: `UPDATE historia_quadrinho 
-              SET img_url="?", data_publicacao=?, titulo="?"
+              SET img_url=?, data_publicacao=?, titulo=?
               WHERE id_hq=?;`,
     deleteById: `DELETE FROM historia_quadrinho WHERE id_hq=?;`
   };
